b33f1e64655e4aa8b8fcc7d8dabf4060fd07e17e
luke20210906 <kk1023du@gmail.com>
2021-11-08 23:44:40 +0900

    Updating Knn.h and Implementing Knn.cc

--- Knn.h
+++ Knn.h
@@ -2,13 +2,15 @@
 #define Knn_H
 
 #include <vector>
+#include <utility>
+#include <cmath>
 
 class Knn {
     private:
         std::vector<std::vector<double>> double_table;
         int dimension;
         std::string metric;
-        void sort(std::vector<double> &vec, std::vector<int> &ret);
+        void sort(std::vector<pair<int, double>> &result, int _k);
 
     public:
         Knn();
@@ -17,14 +19,14 @@ class Knn {
         int get_dim();
         void set_dim(int dim);
         std::string get_metric();
-        void set_metric(std::string met);
+        void set_metric(std::string met);//Euclidean, Manhattan 
 
         double distance(std::vector<double> v1, std::vector<double> v2, std::string met);
 
         //for vector consisting of double.
         void AddDouble(std::vector<double> data); //function that add a vector(double) into table;
-        void SearchByVector(const std::vector<double> target, int k, vector<int> &result); //function that search the nearest k vectors. it returns vectors' id.
-        void SearchByVector(const int target, int k, vector<int> &result); //function that search the nearest k vectors. it returns vectors' id.
+        void SearchByVector(const std::vector<double> target, int k, std::vector<pair<int, double>> &result); //function that search the nearest k vectors. it returns vectors' id.
+        void SearchById(const int target, int k, std::vector<pair<int, double>> &result); //function that search the nearest k vectors. it returns vectors' id.
 
 };
 
