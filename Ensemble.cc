57d8da985730299cd16c36c74182a942a9cc6937
zinc <ghdwlsrl3229@naver.com>
2021-11-10 09:49:45 +0900

    Implement CombSum & CombMNZ algorithm for Ensemble Add is_similarity option at AddRanking method (for distinguish between distance and similarity)
    source : https://ccc.inaoep.mx/~villasen/bib/Performance%20prediction%20of%20data%20fusion%20for%20IR.pdf

--- Ensemble.cc
+++ Ensemble.cc
@@ -7,12 +7,19 @@ void Ensemble::AddRanking(std::vector<int> ranking) {
     ranking_list.push_back(ranking);
 }
 
-void Ensemble::AddRanking(std::vector<std::pair<int, double>> ranking) {
-    ranking_list_with_weight.push_back(ranking);
+void Ensemble::AddRanking(std::vector<std::pair<int, double>> ranking, bool is_similarity) {
+    if (is_similarity) ranking_list_with_weight.push_back(ranking);
+    else {
+        for (auto &i : ranking) {
+            i.second = 1.0 / (1 + i.second);
+        }
+    }
 }
 
 void Ensemble::GetResult(int k, std::string policy, std::vector<int>& result) {
     if (policy == "RRF") ReciprocalRankFusion(k, result);
+    else if (policy == "CombMNZ") CombMNZ(k, result);
+    else if (policy == "CombSum") CombSum(k, result);
 }
 
 void Ensemble::ReciprocalRankFusion(int k, std::vector<int>& result) {
@@ -34,6 +41,59 @@ void Ensemble::ReciprocalRankFusion(int k, std::vector<int>& result) {
     result.resize(k);
     for (int i = 0; i < k; i++) {
         if (i < (int)rrf_res.size()) result[i] = rrf_res[i].second;
+        else result[i] = -1;
+    }
+}
+
+void Ensemble::CombMNZ(int k, std::vector<int>& result) {
+    const int K = 60;
+    std::map<int, double> sim_score;
+    std::map<int, int> num_nonzero_score;
+    for (const auto& ranking : ranking_list_with_weight) {
+        for (const auto& i : ranking) {
+            sim_score[i.first] += (i.second - ranking.back().second) / (ranking[0].second - ranking.back().second);
+            if (i.second != ranking.back().second) num_nonzero_score[i.first]++;
+        }
+    }
+    for (const auto& ranking : ranking_list) {
+        double max_sim = 1.0 / K, min_sim = 1.0 / (K + ranking.size() - 1);
+        for (int i = 0; i < (int)ranking.size(); i++) {
+            sim_score[ranking[i]] += (1.0 / (K + i) - min_sim) / (max_sim - min_sim);
+            if (1.0 / (K + i) != min_sim) num_nonzero_score[ranking[i]]++;
+        }
+    }
+    
+    std::vector<std::pair<double, int>> csum_res;
+    for (auto s : sim_score) csum_res.push_back({s.second * num_nonzero_score[s.first], s.first});
+    std::sort(csum_res.rbegin(), csum_res.rend());
+    result.resize(k);
+    for (int i = 0; i < k; i++) {
+        if (i < (int)csum_res.size()) result[i] = csum_res[i].second;
+        else result[i] = -1;
+    }
+}
+
+void Ensemble::CombSum(int k, std::vector<int>& result) {
+    const int K = 60;
+    std::map<int, double> sim_score;
+    for (const auto& ranking : ranking_list_with_weight) {
+        for (const auto& i : ranking) {
+            sim_score[i.first] += (i.second - ranking.back().second) / (ranking[0].second - ranking.back().second);
+        }
+    }
+    for (const auto& ranking : ranking_list) {
+        double max_sim = 1.0 / K, min_sim = 1.0 / (K + ranking.size() - 1);
+        for (int i = 0; i < (int)ranking.size(); i++) {
+            sim_score[ranking[i]] += (1.0 / (K + i) - min_sim) / (max_sim - min_sim);
+        }
+    }
+
+    std::vector<std::pair<double, int>> csum_res;
+    for (auto s : sim_score) csum_res.push_back({s.second, s.first});
+    std::sort(csum_res.rbegin(), csum_res.rend());
+    result.resize(k);
+    for (int i = 0; i < k; i++) {
+        if (i < (int)csum_res.size()) result[i] = csum_res[i].second;
         else result[i] = -1;
     }
 }
--- Ensemble_Test.cc
+++ Ensemble_Test.cc
@@ -13,5 +13,18 @@ int main(){
 
     cout << "RRF ranking : ";
     for (int i : res) cout << i << ' ';
-    cout<<'\n';
+    cout << '\n';
+
+    en.AddRanking({{1, 0.8}, {2, 0.5}, {5, 0.45}, {4, 0.44}, {3, 0.41}, {6, 0.21}, {7, 0.11}});
+    en.AddRanking({{4, 0.02}, {1, 0.05}, {6, 0.1}, {3, 0.2}, {5, 1}, {2, 4}, {7, 5.6}}, true);
+
+    en.GetResult(7, "CombSum", res);
+    cout << "CombSum ranking: ";
+    for (int i : res) cout << i << ' ';
+    cout << '\n';
+
+    en.GetResult(7, "CombMNZ", res);
+    cout << "CombMNZ ranking: ";
+    for (int i : res) cout << i << ' ';
+    cout << '\n';
 }
