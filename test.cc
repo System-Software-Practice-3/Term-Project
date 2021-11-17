fca332f04754a3b64ee01e8d999b1b5901bf8812
zinc <ghdwlsrl3229@naver.com>
2021-11-17 22:00:47 +0900

    Book Recommendation test using TextRec & Ensemble class

---
+++ test.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <locale>
+#include <kiwi/Kiwi.h>
+
+#include "Text2Vec.h"
+#include "Ensemble.h"
+using namespace std;
+
+const string BOOK_PATH = "/mnt/c/Users/user/Desktop/SSW3/book_text";
+const int MAX_BOOK = 100;
+
+int main(){
+    cbr::TextRec tr;
+    ifstream metafin(BOOK_PATH + "/metadata.txt");
+    istreambuf_iterator<char> begin(metafin), end;
+    string meta = string(begin, end);
+    vector<string> titles = cbr::split(meta, {u8","});
+
+    for (int i = 0; i < MAX_BOOK; i++) {
+        ifstream fin(BOOK_PATH + "/" + to_string(i+1) + ".txt");
+        istreambuf_iterator<char> begin(fin), end;
+        tr.AddData(kiwi::utf8To16(string(begin, end)));
+        fin.close();
+    }
+
+    int target; cin>>target;
+    cout << titles[target - 1] << '\n';
+    vector<pair<int, double>> result1, result2;
+    vector<int> final_result;
+
+    tr.SetConfig({{"ngram_range", "{1, 2}"}});
+    tr.Build("tf-idf");
+    tr.GetRankingList(target - 1, 20, result1);
+
+    tr.Build("jaccard");
+    tr.GetRankingList(target - 1, 20, result2);
+    
+    cout << "tf-idf\n";
+    for (int i = 0; i < 20; i++) {
+        int idx = result1[i].first;
+        cout << i + 1 << ". ";
+        cout << titles[idx] << ' ' << idx + 1  << ' ' << result1[i].second << '\n';
+    }
+
+   cout << "jaccard\n";
+    for (int i = 0; i < 20; i++) {
+        int idx = result2[i].first;
+        cout << i + 1 << ". ";
+        cout << titles[idx] << ' ' << idx + 1  << ' ' << result2[i].second << '\n';
+    }
+
+    cbr::Ensemble en;
+    en.AddRanking(result1);
+    en.AddRanking(result2);
+    en.GetResult(20, "CombSum", final_result);
+
+    cout << "Ensemble Result : \n";
+    for (int i = 0; i < 20; i++) {
+        int idx = final_result[i];
+        cout << i + 1 << ". ";
+        cout << titles[idx] << ' ' << idx + 1  << '\n';
+    }
+
+    return 0;
+}
