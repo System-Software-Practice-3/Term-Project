d0b73e1c4d61a8be40f45ec9c9d4a92699e7055b
ohpurumoe <82622524+ohpurumoe@users.noreply.github.com>
2021-11-23 11:01:23 +0900

    Delete histogram.h

--- histogram.h
+++
@@ -1,7 +0,0 @@
-#ifndef HISTO
-#define HISTO
-
-void calculate_histo(img2RGB input,std::vector<img2RGB> stored_picture);
-std::vector<img2RGB> histo_similarity(std::vector<img2RGB> stored_picture, int choose_num);
-
-#endif
